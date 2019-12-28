bbd46ec93d9f5b13d7b6268dd4a8955c6127afad
Kevinabcabc <1356075038@qq.com>
2019-12-28 14:26:21 +0800

    hanege

--- src/ajax/api.ts
+++ src/ajax/api.ts
@@ -1,12 +1,17 @@
-const BassUrl = 'http://localhost:3000'
-const API={
+enum API{
+    
+    LOGIN_API =  '/manager/api/login',
 
-    LOGIN_API : BassUrl + '/manager/api/login',
-    ANALYSIS_USER_TODAY_DATA :  BassUrl + '/manager/api/analysis/user/today',
-    ANALYSIS_USER_SELECT_DATA :  BassUrl + '/manager/api/analysis/user/',
-    USER_ADDUSER :   BassUrl + '/api/user/adduser',
-    USER_FINDUSER :   BassUrl +  '/api/user/findAllUser',
-    USER_EDIT_ROLE :   BassUrl +  '/api/user/saveupdateuser'
+
+
+    ANALYSIS_USER_TODAY_DATA =  '/manager/api/analysis/user/today',
+    ANALYSIS_USER_SELECT_DATA =  '/manager/api/analysis/user/',
+
+
+
+
+
+
 }
 
 export default API;
